9bc304d34e7df62952feb30a5661bace576cd3d2
Artem <sevruk04@bk.ru>
2023-11-30 11:51:30 +0300

    add dev-server and css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry : "./src/index.js",
   output : {
@@ -6,4 +7,20 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 8000,
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader" , "css-loader"]
+      }
+    ]
+  }
 }
